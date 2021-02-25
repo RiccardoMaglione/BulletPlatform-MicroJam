27926e596ea85d321b139332a2cdb20c4dca062b
Maglione Riccardo <riccardo99maglione@gmail.com>
2021-02-25 10:40:41 +0100

    Add bullet scripts

---
+++ Assets/Scripts/Player/BulletInstantiate.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletInstantiate : MonoBehaviour
+{
+    public GameObject Bullet;
+    public bool CanSpawn = true;
+    public float Delay = 1.5f;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P) && CanSpawn == true)
+        {
+            BInst();
+        }
+    }
+
+    public void BInst()
+    {
+        GameObject TempBullet = Instantiate(Bullet, transform.position, transform.rotation);
+        CanSpawn = false;
+        StartCoroutine(CooldownSpawnBullet());
+    }
+
+    public IEnumerator CooldownSpawnBullet()
+    {
+        yield return new WaitForSeconds(Delay);
+        CanSpawn = true;
+    }
+}
---
+++ Assets/Scripts/Player/BulletMove.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletMove : MonoBehaviour
+{
+    public float speed;
+
+    void Update()
+    {
+        transform.position += transform.right * Time.deltaTime * speed;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Wall")
+        {
+            this.enabled = false;
+        }
+    }
+}
